6e457fba42302067e6e34f40066803e19abe8269
Luis Gutiérrez <luisghtz@outlook.com>
2023-01-31 13:37:37 -0600

    Create picturte and sources

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterContentInit, Input } from '@angular/core';
+import { Source } from 'src/app/models/source.model';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
-  styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements AfterContentInit {
+  @Input() alt: string = '';
+  @Input() src: string = '';
+  sources: Source[] = [];
 
-  constructor() { }
+  ngAfterContentInit(): void {
+    const splitSrc = this.src.split('/');
+    const fullName = splitSrc.pop();
+    const src = splitSrc.join('/');
+    const splitName = fullName?.split('.') ?? [];
+    const ext = splitName.pop() ?? '';
+    const name = splitName.pop() ?? '';
 
-  ngOnInit(): void {
+    this.sources = this.#getSources(src, name, ext);
   }
 
+  #getSources(src: string, name: string, ext: string): Source[] {
+    return [
+      {
+        src: `${src}/${name}-xs.webp`,
+        media: '(max-width: 425px)',
+      },
+      {
+        src: `${src}/${name}-sm.webp`,
+        media: '(max-width: 1023px)',
+      },
+      {
+        src: `${src}/${name}-md.webp`,
+        media: '(max-width: 1439px)',
+      },
+      {
+        src: `${src}/${name}-lg.webp`,
+        media: '(max-width: 1999px)',
+      },
+      {
+        src: `${src}/${name}-xl.webp`,
+        media: '(min-width: 2000px)',
+      },
+      {
+        src: `${src}/${name}-xs.${ext}`,
+        media: '(max-width: 425px)',
+      },
+      {
+        src: `${src}/${name}-sm.${ext}`,
+        media: '(max-width: 1023px)',
+      },
+      {
+        src: `${src}/${name}-md.${ext}`,
+        media: '(max-width: 1439px)',
+      },
+      {
+        src: `${src}/${name}-lg.${ext}`,
+        media: '(max-width: 1999px)',
+      },
+      {
+        src: `${src}/${name}-xl.${ext}`,
+        media: '(min-width: 2000px)',
+      },
+    ];
+  }
 }
